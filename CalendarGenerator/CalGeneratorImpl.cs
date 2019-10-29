6ee59cbed7f4d9dbe9f7098e7777080436a33ae8
Max <maksymilianrozanski@gmail.com>
2019-10-29 16:17:48 +0100

    add CalendarGenerator Class Library project

---
+++ CalendarGenerator/CalGeneratorImpl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace CalendarGenerator
+{
+    public class CalGeneratorImpl : ICalGenerator
+    {
+        public string GenerateICalCalendar(string copiedText)
+        {
+            // TODO: not implemented yet
+            return "not implemented yet";
+        }
+    }
+}
---
+++ CalendarGenerator/ICalGenerator.cs
@@ -0,0 +1,7 @@
+namespace CalendarGenerator
+{
+    public interface ICalGenerator
+    {
+        string GenerateICalCalendar(string copiedText);
+    }
+}
