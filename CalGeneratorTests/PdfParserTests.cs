42f2cd98e09edf9ad6f2d5be1cadab3104ca5c17
Max <maksymilianrozanski@gmail.com>
2019-11-04 21:00:49 +0100

    add method parsing words to list of day-string items

--- CalGeneratorTests/PdfParserTests.cs
+++ CalGeneratorTests/PdfParserTests.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections.Generic;
 using CalendarGenerator.PdfParse;
 using NUnit.Framework;
 
@@ -12,6 +14,23 @@ namespace CalGeneratorTests
             var input = expectedOutput[0] + " " + expectedOutput[1] + "\n" + expectedOutput[2] + " " +
                         expectedOutput[3];
             var result = PdfParser.RawTextToWords(input);
+            Assert.AreEqual(expectedOutput, result);
+        }
+
+        [Test]
+        public void WordsToDayItemsTest()
+        {
+            var expectedOutput = new List<string>
+            {
+                "Data Zajęć: 2019-10-04 piątek 17:30 19:00 2h00m dr Name Surname Advanced Math Wyk W/1/Web F Toronto Egzamin 19:15 20:45 2h00m doc. dr John Smiths Modern History of Poland Wyk W/1/Web F Praga Egzamin",
+                "Data Zajęć: 2019-10-05 sobota 8:00 9:30 2h00m mgr Marry Smiths-Blue Chemistry Cw 12 Blue 2/IEN F Sztokholm Zaliczenie ocena 11:20 14:30 4h00m doc. dr John Black Physics Wyk W/2/W F Toronto Egzamin",
+                "Data Zajęć: 2019-10-06 niedziela 8:00 9:30 2h00m mgr Jack Green Wzorce projektowe Lab lab/WebN F Montreal Zaliczenie ocena 9:40 11:10 2h00m mgr inż. Thomas Orange Biology and Geography Lab lab15/2/WebN F Los Angeles Zaliczenie ocena 14:40 16:10 2h00m dr George White Geology Lab lab51/22/WebN F San Francisco Zaliczenie ocena 16:20 17:50 2h00m mgr Richard White Basic Mathematics Lab lab22/22/WebN F Chicago Zaliczenie ocena 18:00 19:30 2h00m mgr David Smith Civil Engineering Lab lab3/3/WebN F Nowy Jork Zaliczenie ocena 19:40 21:10 2h00m mgr Jacob Brown Advanced Mathematics Konw konw/2/WebN F Toronto Zaliczenie ocena",
+                "Data Zajęć: 2019-10-18 piątek 17:30 19:00 2h00m dr Jacob Brown Advanced Physics Wyk W/2/WebN F Toronto Egzamin 19:15 20:45 2h00m dr inż. Thomas Blue Geology Konw konw/2/WebN F Praga Zaliczenie ocena"
+            };
+            var wordsInput =
+                "Czas od Czas do Liczba godzin Prowadzący Przedmiot Forma zaj. Grupy Sala Forma zaliczenia Uwagi Data Zajęć: 2019-10-04 piątek 17:30 19:00 2h00m dr Name Surname Advanced Math Wyk W/1/Web F Toronto Egzamin 19:15 20:45 2h00m doc. dr John Smiths Modern History of Poland Wyk W/1/Web F Praga Egzamin Data Zajęć: 2019-10-05 sobota 8:00 9:30 2h00m mgr Marry Smiths-Blue Chemistry Cw 12 Blue 2/IEN F Sztokholm Zaliczenie ocena 11:20 14:30 4h00m doc. dr John Black Physics Wyk W/2/W F Toronto Egzamin Data Zajęć: 2019-10-06 niedziela 8:00 9:30 2h00m mgr Jack Green Wzorce projektowe Lab lab/WebN F Montreal Zaliczenie ocena 9:40 11:10 2h00m mgr inż. Thomas Orange Biology and Geography Lab lab15/2/WebN F Los Angeles Zaliczenie ocena 14:40 16:10 2h00m dr George White Geology Lab lab51/22/WebN F San Francisco Zaliczenie ocena 16:20 17:50 2h00m mgr Richard White Basic Mathematics Lab lab22/22/WebN F Chicago Zaliczenie ocena 18:00 19:30 2h00m mgr David Smith Civil Engineering Lab lab3/3/WebN F Nowy Jork Zaliczenie ocena 19:40 21:10 2h00m mgr Jacob Brown Advanced Mathematics Konw konw/2/WebN F Toronto Zaliczenie ocena Data Zajęć: 2019-10-18 piątek 17:30 19:00 2h00m dr Jacob Brown Advanced Physics Wyk W/2/WebN F Toronto Egzamin 19:15 20:45 2h00m dr inż. Thomas Blue Geology Konw konw/2/WebN F Praga Zaliczenie ocena "
+                    .Split(" ");
+            var result = PdfParser.WordsToDayItems(wordsInput);
             Assert.AreEqual(expectedOutput, result);
         }
     }
--- CalendarGenerator/PdfParse/PdfParser.cs
+++ CalendarGenerator/PdfParse/PdfParser.cs
@@ -1,6 +1,10 @@
+using System.Collections.Generic;
 using System.Runtime.CompilerServices;
+using System.Text;
+using System.Text.RegularExpressions;
 
-[assembly: InternalsVisibleTo("CalGeneratorTests")]
+[assembly: InternalsVisibleTo("CalGeneratorTests"), InternalsVisibleTo("ConsoleCalGenerator")]
+
 namespace CalendarGenerator.PdfParse
 {
     internal class PdfParser
@@ -9,6 +13,56 @@ namespace CalendarGenerator.PdfParse
         {
             var removedBreakLines = input.Replace("\n", " ");
             return removedBreakLines.Split(" ");
+        }
+
+        internal static List<string> WordsToDayItems(string[] words)
+        {
+            var stringBuilder = new StringBuilder();
+            var dayItems = new List<string>();
+            var dayPattern = "Data";
+            var hourPattern = "\\d?\\d:\\d\\d";
+
+            int wordCounter = 0;
+
+            foreach (var word in words)
+            {
+                var dayMatcher = Regex.Match(word, dayPattern);
+                var hourMatcher = Regex.Match(word, hourPattern);
+
+                if (dayMatcher.Success)
+                {
+                    if (stringBuilder.Length != 0) dayItems.Add(stringBuilder.ToString().Trim());
+                    stringBuilder.Clear();
+                    wordCounter = 0;
+                    stringBuilder.Append(word);
+                    stringBuilder.Append(" ");
+                    wordCounter++;
+                }
+                else if (hourMatcher.Success && wordCounter > 0)
+                {
+                    stringBuilder.Append(word);
+                    stringBuilder.Append(" ");
+                    wordCounter++;
+                }
+                else if (hourMatcher.Success)
+                {
+                    if (stringBuilder.Length != 0) dayItems.Add(stringBuilder.ToString().Trim());
+                    stringBuilder.Clear();
+                    wordCounter = 0; //TODO: check use of wordCounter
+                    stringBuilder.Append(word);
+                    stringBuilder.Append(" ");
+                    wordCounter++;
+                }
+                else if (wordCounter > 0)
+                {
+                    stringBuilder.Append(word);
+                    stringBuilder.Append(" ");
+                    wordCounter++;
+                }
+            }
+
+            dayItems.Add(stringBuilder.ToString().Trim());
+            return dayItems;
         }
     }
 }
