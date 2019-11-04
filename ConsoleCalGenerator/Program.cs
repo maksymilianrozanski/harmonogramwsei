2e77b04b63cefa6d9f880791e017c8739c789403
Max <maksymilianrozanski@gmail.com>
2019-11-04 19:42:08 +0100

    add Console app project reading text from pdf file

---
+++ ConsoleCalGenerator/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using CalendarGenerator.PdfRead;
+
+namespace ConsoleCalGenerator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter pdf file path.");
+            var path = Console.ReadLine();
+            var reader = new PdfTextReader();
+            
+            using var stream = File.Open(path, FileMode.Open, FileAccess.Read);
+            var extractedText = reader.GetTextFromAllPages(stream);
+            Console.WriteLine(extractedText);
+        }
+    }
+}
