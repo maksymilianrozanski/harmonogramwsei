c483c63eb0689eb5b310a0d4cc677d39666f9ed9
Max <maksymilianrozanski@gmail.com>
2019-11-04 17:18:12 +0100

    add GenerateICalCalendar method taking FileStream as argument to ICalGenerator interface

--- CalendarGenerator/CalGeneratorImpl.cs
+++ CalendarGenerator/CalGeneratorImpl.cs
@@ -1,10 +1,17 @@
 ﻿using System;
+using System.IO;
 
 namespace CalendarGenerator
 {
     public class CalGeneratorImpl : ICalGenerator
     {
         public string GenerateICalCalendar(string pdfFilePath)
+        {
+            // TODO: not implemented yet
+            return "not implemented yet";
+        }
+
+        public string GenerateICalCalendar(FileStream pdfFileStream)
         {
             // TODO: not implemented yet
             return "not implemented yet";
--- CalendarGenerator/ICalGenerator.cs
+++ CalendarGenerator/ICalGenerator.cs
@@ -1,7 +1,11 @@
+using System.IO;
+
 namespace CalendarGenerator
 {
     public interface ICalGenerator
     {
         string GenerateICalCalendar(string pdfFilePath);
+
+        string GenerateICalCalendar(FileStream pdfFileStream);
     }
 }
