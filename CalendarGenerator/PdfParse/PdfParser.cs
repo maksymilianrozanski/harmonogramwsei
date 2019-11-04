faf1b99c4ed23a4aabd5bebb119dbcf3e2f12a3d
Max <maksymilianrozanski@gmail.com>
2019-11-04 21:23:55 +0100

    refactor WordsToDayItems method

--- CalendarGenerator/PdfParse/PdfParser.cs
+++ CalendarGenerator/PdfParse/PdfParser.cs
@@ -19,45 +19,22 @@ namespace CalendarGenerator.PdfParse
         {
             var stringBuilder = new StringBuilder();
             var dayItems = new List<string>();
-            var dayPattern = "Data";
-            var hourPattern = "\\d?\\d:\\d\\d";
-
-            int wordCounter = 0;
+            const string dayPattern = "Data";
+            var afterTableHeader = false;
 
             foreach (var word in words)
             {
                 var dayMatcher = Regex.Match(word, dayPattern);
-                var hourMatcher = Regex.Match(word, hourPattern);
-
                 if (dayMatcher.Success)
                 {
+                    afterTableHeader = true;
                     if (stringBuilder.Length != 0) dayItems.Add(stringBuilder.ToString().Trim());
                     stringBuilder.Clear();
-                    wordCounter = 0;
-                    stringBuilder.Append(word);
-                    stringBuilder.Append(" ");
-                    wordCounter++;
+                    stringBuilder.Append(word + " ");
                 }
-                else if (hourMatcher.Success && wordCounter > 0)
+                else if (afterTableHeader)
                 {
-                    stringBuilder.Append(word);
-                    stringBuilder.Append(" ");
-                    wordCounter++;
-                }
-                else if (hourMatcher.Success)
-                {
-                    if (stringBuilder.Length != 0) dayItems.Add(stringBuilder.ToString().Trim());
-                    stringBuilder.Clear();
-                    wordCounter = 0; //TODO: check use of wordCounter
-                    stringBuilder.Append(word);
-                    stringBuilder.Append(" ");
-                    wordCounter++;
-                }
-                else if (wordCounter > 0)
-                {
-                    stringBuilder.Append(word);
-                    stringBuilder.Append(" ");
-                    wordCounter++;
+                    stringBuilder.Append(word + " ");
                 }
             }
 
