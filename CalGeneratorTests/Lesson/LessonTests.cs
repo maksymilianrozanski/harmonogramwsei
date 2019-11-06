b52a47fea3b612b9a548e23e1044681a040a3994
Max <maksymilianrozanski@gmail.com>
2019-11-06 14:49:43 +0100

    modify ExtractDateTest input

--- CalGeneratorTests/Lesson/LessonTests.cs
+++ CalGeneratorTests/Lesson/LessonTests.cs
@@ -8,7 +8,7 @@ namespace CalGeneratorTests.Lesson
         [Test]
         public void ExtractDateTest()
         {
-            var input = "Data Zajęć: 2019-10-04 piątek 17:30 19:00";
+            var input = "Data Zajęć: 2019-10-04 piątek";
             var expected = "2019-10-04";
             var result = LessonText.ExtractDate(input);
             Assert.AreEqual(expected, result);
