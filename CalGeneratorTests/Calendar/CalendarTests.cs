59bbbf8f9400c481f3dfa56fe698a57a0f56e1f6
Max <maksymilianrozanski@gmail.com>
2019-11-11 14:46:26 +0100

    clean up CalendarTests

--- CalGeneratorTests/Calendar/CalendarTests.cs
+++ CalGeneratorTests/Calendar/CalendarTests.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Linq;
-using System.Reactive.Linq;
-using System.Text.RegularExpressions;
 using CalendarGenerator.PdfParse;
 using NUnit.Framework;
 
