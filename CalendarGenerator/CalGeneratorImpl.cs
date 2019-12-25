a6da3f5a927783086ea79b976b29b91eecfb5bcb
Max <maksymilianrozanski@gmail.com>
2019-12-25 16:32:05 +0100

    add xml comments

--- CalendarGenerator/CalGeneratorImpl.cs
+++ CalendarGenerator/CalGeneratorImpl.cs
@@ -5,6 +5,11 @@ namespace CalendarGenerator
 {
     public class CalGeneratorImpl : ICalGenerator
     {
+        /// <summary>
+        /// Generate calendar text as string in ical format. 
+        /// </summary>
+        /// <param name="pdfFilePath">Path of the file containing schedule</param>
+        /// <returns></returns>
         public string GenerateICalCalendar(string pdfFilePath)
         {
             var reader = new PdfTextReader();
@@ -14,6 +19,11 @@ namespace CalendarGenerator
             return iCalendarText;
         }
 
+        /// <summary>
+        /// Generate calendar text as string in ical format.
+        /// </summary>
+        /// <param name="pdfFileStream">FileStream of file containing schedule</param>
+        /// <returns></returns>
         public string GenerateICalCalendar(FileStream pdfFileStream)
         {
             var reader = new PdfTextReader();
--- CalendarGenerator/ICalGenerator.cs
+++ CalendarGenerator/ICalGenerator.cs
@@ -4,8 +4,18 @@ namespace CalendarGenerator
 {
     public interface ICalGenerator
     {
+        /// <summary>
+        /// Generate calendar text as string in ical format. 
+        /// </summary>
+        /// <param name="pdfFilePath">Path of the file containing schedule</param>
+        /// <returns></returns>
         string GenerateICalCalendar(string pdfFilePath);
 
+        /// <summary>
+        /// Generate calendar text as string in ical format.
+        /// </summary>
+        /// <param name="pdfFileStream">FileStream of file containing schedule</param>
+        /// <returns></returns>
         string GenerateICalCalendar(FileStream pdfFileStream);
     }
 }
